0699a2c7e2bd74c91b9d14ce0214745a3505af7d
Julie Reynal <rjulie.pro@gmail.com>
2023-01-29 23:05:54 +0100

    actions controller lineItems done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,15 @@ module ApplicationHelper
       end
     end
   end
+
+  def nested_dom_id(*args)
+    args.map { |arg| arg.respond_to?(:to_key) ? dom_id(arg) : arg }.join("_")
+  end
+
+  # With the helper we can generate and read our dom_ids
+  # line_item_date = LineItemDate.find(1)
+  # nested_dom_id(line_item_date, LineItem.new)
+  # => line_item_date_1_new_line_item
+  # nested_dom_id(line_item_date, "line_items")
+  # => line_item_date_1_line_items
 end
